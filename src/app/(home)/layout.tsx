0101e00373f1a6436282586203a23c00731b8a09
Ritankar Bhattacharjee <ritankar.bhattacharjee80@gmail.com>
2025-08-22 19:19:45 +0530

    navbar desgined for desktop

---
+++ src/app/(home)/layout.tsx
@@ -0,0 +1,16 @@
+import Navbar from "@/components/navbar";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const Layout = ({ children }: Props) => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      {children}
+    </div>
+  );
+};
+
+export default Layout;
